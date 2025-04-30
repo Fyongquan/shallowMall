fff5fd4ec715dd1f22f48d314f05654b9900b191
FuYongquan <1304184660@qq.com>
2025-05-01 00:45:06 +0800

    day5-网关统一配置跨域和完善45节的查询网关配置
    --对应课程(47-48)
    47、
    1、在shallowMall-gateway中新增过滤器ShallowMallCorsConfiguration
    @Configuration
    public class ShallowMallCorsConfiguration {
        @Bean
        public CorsWebFilter corsWebFilter() {
            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
            CorsConfiguration config = new CorsConfiguration();
            //1、配置跨域
            config.addAllowedHeader("*");
            config.addAllowedMethod("*");
            config.addAllowedOrigin("*");
            config.setAllowCredentials(true);
            source.registerCorsConfiguration("/**", config);
            return new CorsWebFilter(source);
        }
    }
    2、注释renren-fast自带的跨域配置
    3、然后就能够成功登录了
    ﻿
    48、
    1、在shallowMall-gateway中添加网关路由，并放到/api的上面，防止被/api拦截请求
            - id: product_route
              uri: lb://shallowMall-product
              predicates:
                - Path=/api/product/**
              filters:
                - RewritePath=/api/(?<segment>.*),/$\{segment}
    当请求路径匹配到/api/product/**时，将请求负载均衡转发到shallowMall-gateway的服务实例上，将
    http://localhost:88/api/product/category/list/tree -> http://localhost:10000/product/category/list/tree
    2、然后访问商品服务的分类维护就能够访问到数据了

---
+++ shallowMall-gateway/src/main/java/com/fyq/shallowmall/gateway/config/ShallowMallCorsConfiguration.java
@@ -0,0 +1,28 @@
+package com.fyq.shallowmall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class ShallowMallCorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        //1、配置跨域
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
